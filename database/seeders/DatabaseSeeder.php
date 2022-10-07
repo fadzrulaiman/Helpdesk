33225f7569586c7668a8bbb33cccc3cc59ce8622
fadzrulaiman <daniel.fadzrul@gmail.com>
2022-10-07 16:03:52 +0800

    Update Department Table Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StatusesTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
         $this->call(LocationsTableSeeder::class);
+        $this->call(DepartmentTableSeeder::class);
     }
 }
