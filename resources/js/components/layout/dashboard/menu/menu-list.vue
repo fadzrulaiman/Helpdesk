1a009d4f9e74d32d99cc3ff167584e542c86da7b
fadzrulaiman <daniel.fadzrul@gmail.com>
2022-10-20 12:00:55 +0800

    Update menu list

--- resources/js/components/layout/dashboard/menu/menu-list.vue
+++ resources/js/components/layout/dashboard/menu/menu-list.vue
@@ -49,6 +49,13 @@
             to="/dashboard/admin/labels"
         ></menu-item>
         <menu-item
+            v-if="$store.state.permissions && $store.state.permissions['App.Http.Controllers.Api.Dashboard.Admin.LocationController']"
+            :label="$t('Location')"
+            :mobile="mobile"
+            icon="font-awesome.tasks-regular"
+            to="/dashboard/admin/locations"
+        ></menu-item>
+        <menu-item
             v-if="$store.state.permissions && $store.state.permissions['App.Http.Controllers.Api.Dashboard.Admin.StatusController']"
             :label="$t('Statuses')"
             :mobile="mobile"
