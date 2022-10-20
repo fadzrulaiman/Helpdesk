c09c458b5ff1e87d33832f1a6c6f29430a6a116f
fadzrulaiman <daniel.fadzrul@gmail.com>
2022-10-20 16:20:55 +0800

    Update Ticket Controller

--- app/Http/Controllers/Api/Ticket/TicketController.php
+++ app/Http/Controllers/Api/Ticket/TicketController.php
@@ -73,6 +73,7 @@ class TicketController extends Controller
         $ticket->uuid = Str::uuid();
         $ticket->subject = $request->get('subject');
         $ticket->status_id = 1;
+        $ticket->priority_id = 2;
         if ($request->has('department_id')) {
             $ticket->department_id = $request->get('department_id');
         }
